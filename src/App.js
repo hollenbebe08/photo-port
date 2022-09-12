3d1660734d5cd021328e9b2bd4ae2f949bb41048
Betsy <betsy.griffith12@gmail.com>
2022-09-12 14:04:06 -0400

    updated Nav bar with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
-import './App.css';
-import About from './components/About';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
@@ -16,18 +15,31 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div className="App">
       <Nav
-        categories = {categories}
-        setCurrentCategory = {setCurrentCategory}
-        currentCategory = {currentCategory}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+          {/* if contact tab is not selected then show gallery and about section */}
+          {!contactSelected ? (
+            //  the <> and </> are React fragments—a shorthand abbreviation for <React.Fragment></React.Fragment>.
+            <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+            </>
+          // the ):( is the same as an else statement
+          ) : (
+            <ContactForm></ContactForm>
+          )}
         </div>
       </main>
     </div>
